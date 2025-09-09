9ec86a37d858b55cbe9157a0be54eedfe70af0ee
Nurudeen <nuksi911@gmail.com>
2025-09-09 13:01:22 +0100

    update build configuration

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -135,7 +135,7 @@ export class Resume extends Component {
                 </small>
               </a>
               <a
-                https:href="https://drive.google.com/file/d/131I93tP-4xfOeN0zQ13q0lDcZrNJ0jjc/view?usp=sharing"
+                href="https://drive.google.com/file/d/131I93tP-4xfOeN0zQ13q0lDcZrNJ0jjc/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
               >
